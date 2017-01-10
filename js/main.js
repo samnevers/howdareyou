d0f7072aaae0e87b36aaaef38eb5d0256d40a734
samnevers <thesamanthaki@gmail.com>
2017-01-10 16:52:44 +1100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,109 @@
+
+var students = {
+    ahi: [
+        ["YOU OWE ME YOUR FAVE MEMORY OR GOODBYE MESSAGE MOTHERFUCKER"],
+    ],
+    drew: [
+        ["YOU OWE ME YOUR FAVE MEMORY OR GOODBYE MESSAGE MOTHERFUCKER"]
+    ],
+    emmanuel: [
+        ["badger can't hear me whispering to him"]
+    ],
+    esther: [
+        ["YOU OWE ME YOUR FAVE MEMORY OR GOODBYE MESSAGE MOTHERFUCKER"]
+
+    ],
+    ewelina: [
+        ["What do you mean, Ewelina?"],
+        ["What?!"]
+    ],
+    mario: [
+        "I'm about to press BADGER Destroy_all from my database"
+    ],
+    rhys: [
+        ["YOU OWE ME YOUR FAVE MEMORY OR GOODBYE MESSAGE MOTHERFUCKER"]
+    ],
+    rozen: [
+        ["Rozen, you can't hide behind your laptop screen!"]
+
+    ],
+    sam: [
+        ["YOU OWE ME YOUR FAVE MEMORY OR GOODBYE MESSAGE MOTHERFUCKER"]
+    ],
+    steve: [
+        ["YOU OWE ME YOUR FAVE MEMORY OR GOODBYE MESSAGE MOTHERFUCKER"]
+    ],
+    tim: [
+        ["YOU OWE ME YOUR FAVE MEMORY OR GOODBYE MESSAGE MOTHERFUCKER"]
+    ],
+    ned: [
+        ["YOU OWE ME YOUR FAVE MEMORY OR GOODBYE MESSAGE MOTHERFUCKER"]
+    ],
+    james: [
+        "YOU OWE ME YOUR FAVE MEMORY OR GOODBYE MESSAGE MOTHERFUCKER"]
+};
+
+var studs = Object.keys(students);
+
+var current;
+var $el;
+
+var showActivities = function(student) {
+
+    $(".overlay").fadeToggle();
+    var name = $(this).attr("id");
+    var activities = students[student];
+    var $heading = $("<h2></h2>");
+    $heading.text(student);
+    current = student;
+    $(".text-block").append($heading);
+    for (var i = 0; i < activities.length; i++) {
+        var $para;
+        if (Array.isArray(activities[i])) {
+            $para = $("<a></a>");
+            $para.text(activities[i][0]);
+            $para.attr("href", activities[i][1]);
+            $para.attr("target", "_blank");
+        } else {
+            $para = $("<p></p>");
+            $para.text(activities[i]);
+        }
+        $(".text-block").append($para);
+    }
+};
+
+$(document).ready(function() {
+
+    $(".photos").on("click", function() {
+        var student = $(this).attr("id");
+        showActivities(student);
+    });
+
+    $(".close").on("click", function() {
+        $(".text-block").empty();
+        $(".overlay").fadeToggle();
+    });
+
+    $(".arrow").on("click", function() {
+        $el = $(this);
+        $(".text-block").empty();
+        $(".overlay").fadeToggle(function () {
+            var currentIndex = studs.indexOf(current);
+            var next;
+            if ($el.hasClass("right")) {
+                if (currentIndex + 1 < studs.length)
+                    next = studs[currentIndex + 1];
+                else {
+                    next = studs[0];
+                }
+            } else {
+                if (currentIndex - 1 >= 0) {
+                    next = studs[currentIndex - 1];
+                } else {
+                    next = studs[studs.length - 1];
+                }
+            }
+            showActivities(next);
+        });
+    });
+});
